6cfeeed9824af85e6ebff1c88648496d72b5cecb
Srihariharan <srihariharan.g@syncfusion.com>
2024-02-08 17:01:25 +0530

    869704: create the sample for compress pdf in Net8.0

--- Compress_PDF_WF/Compress_PDF_WF/Form1.Designer.cs
+++ Compress_PDF_WF/Compress_PDF_WF/Form1.Designer.cs
@@ -3,12 +3,12 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,8 +23,8 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -55,6 +55,7 @@
             this.groupBox3.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.Size = new System.Drawing.Size(500, 524);
             this.SuspendLayout();
             // 
             // compressPDF
@@ -350,4 +351,3 @@
         private System.Windows.Forms.CheckBox incrementalUpdate;
     }
 }
-
